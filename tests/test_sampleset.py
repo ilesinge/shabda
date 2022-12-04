76d54ee54060c4639a82db441b60425b17eafb74
Alexandre G.-Raymond <alex@ndre.gr>
2022-12-04 11:35:00 +0100

    Fix unit tests

--- tests/test_sampleset.py
+++ tests/test_sampleset.py
@@ -3,9 +3,10 @@
 import os
 from shabda.sampleset import SampleSet
 from shabda.sound import Sound
+from pyfakefs.fake_filesystem_unittest import patchfs
 
 
-def test_init_new():
+def test_init_new(fake_filesystem):
     """Initialize non existing sample"""
     SampleSet("test1")
     assert os.path.exists("samples/test1/")
@@ -22,13 +23,13 @@ def test_init_existing(fake_filesystem):
     assert sample.sounds == [2, 3]
 
 
-def test_dir():
+def test_dir(fake_filesystem):
     """Check sample directory"""
     sample = SampleSet("test1")
     assert sample.dir() == "samples/test1"
 
 
-def test_add(mocker):
+def test_add(mocker, fake_filesystem):
     """Add a sample"""
     sound = mocker.Mock()
     sound.id = 4
@@ -36,6 +37,8 @@ def test_add(mocker):
     sound.licensename = "cc0"
     sound.url = "https://sample/"
     sound.file = "samples/tchak/0.wav"
+    sound.gender = None
+    sound.language = None
     sample = SampleSet("test1")
     sample.add(sound)
     assert {
@@ -44,6 +47,8 @@ def test_add(mocker):
         "license": "cc0",
         "url": "https://sample/",
         "file": "samples/tchak/0.wav",
+        "gender": None,
+        "language": None,
     } in sample.sounds
 
 
@@ -72,7 +77,7 @@ def test_clean(fake_filesystem):  # pylint:disable=unused-argument
     assert not os.path.exists("samples/test1/")
 
 
-def test_saveconfig():
+def test_saveconfig(fake_filesystem):
     """Saving sample config file"""
     sample = SampleSet("test1")
     sample.master_id = 1
@@ -98,7 +103,7 @@ def test_saveconfig():
     ]
 
 
-def test_contains():
+def test_contains(fake_filesystem):
     """Checking if sample set contains a sound id"""
     sample = SampleSet("test1")
     sample.master_id = 1
