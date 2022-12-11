9240bfac1fae93760631bfea46a7c92ae95e1f8c
Alexandre G.-Raymond <alex@ndre.gr>
2022-12-11 20:17:43 +0100

    Add speech UI

--- assets/shabda.js
+++ assets/shabda.js
@@ -3,10 +3,15 @@ var root = document.body
 var State = {
     pack: "",
     progress: false,
+    lastretrievedtype: "",
     lastretrieved: "",
     lastreslistcontents: [],
     error: "",
     licenses: ["by", "cc0", "by-nc"],
+    speech: "",
+    language: "uk-UA",
+    gender: "f",
+    tab: "pack",
 
     licensefullname: function (name) {
         switch (name) {
@@ -41,7 +46,15 @@ var State = {
     },
 
     lastreslist: function () {
-        return '!reslist "' + location.href + State.lastretrievedreslist() + (State.licenses.length < 3 ? "?licenses=" + State.licenses.join() : "") + '"'
+        reslist = '!reslist "' + location.href + State.lastretrievedreslist()
+        if (this.lastretrievedtype == "pack" && State.licenses.length < 3) {
+            reslist = reslist + "?licenses=" + State.licenses.join()
+        }
+        if (this.lastretrievedtype == "speech") {
+            reslist = reslist + "?gender=" + State.gender + "&language=" + State.language
+        }
+        reslist = reslist + '"'
+        return reslist
     },
 
     retrieve: function () {
@@ -56,6 +69,7 @@ var State = {
                     State.progress = false
                     if (result.status == "ok") {
                         State.lastretrieved = result.definition
+                        State.lastretrievedtype = "pack"
                         m.request({
                             method: "GET",
                             url: encodeURIComponent(State.lastretrievedreslist()) + '?complete=1&licenses=' + State.licenses.join()
@@ -77,6 +91,7 @@ var State = {
             State.error = "Please enter a pack definition"
         }
     },
+
     copyreslist: function () {
         if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
             if (navigator.clipboard.writeText(State.lastreslist())) {
@@ -87,7 +102,42 @@ var State = {
             State.copied = false
             m.redraw()
         }, 2000)
-    }
+    },
+
+    retrievespeech: function () {
+        if (State.speech) {
+            State.error = ""
+            State.progress = true
+            m.request({
+                method: "GET",
+                url: "/speech/" + encodeURIComponent(State.speech) + '?language=' + State.language + '&gender=' + State.gender
+            })
+                .then(function (result) {
+                    State.progress = false
+                    if (result.status == "ok") {
+                        State.lastretrieved = "speech/" + result.definition
+                        State.lastretrievedtype = "speech"
+                        m.request({
+                            method: "GET",
+                            url: encodeURIComponent(State.lastretrievedreslist()) + '?language=' + State.language + '&gender=' + State.gender
+                        }).then(function (result) {
+                            State.lastreslistcontents = result
+                        })
+                    }
+                    else if (result.status == "empty") {
+                        State.error = "Pack is empty"
+                    }
+                    else {
+                        State.error = "An error occured"
+                    }
+                }).catch(function (error) {
+                    State.error = "An error occured"
+                })
+        }
+        else {
+            State.error = "Please enter a pack definition"
+        }
+    },
 }
 
 var Shabda = {
@@ -106,53 +156,172 @@ var Shabda = {
                 "."]),
 
             m("div",
-                m("input[placeholder=Pack definition]#definition", {
-                    value: State.pack,
-                    oninput: function (e) { State.pack = e.target.value },
-                    onkeyup: function (e) {
-                        if (e.keyCode == 13) {
-                            State.retrieve()
-                        }
-                    }
-                }),
-                m("div#licenses", [
-                    m("input[type=checkbox]#license-cc0", { name: "licenses", value: "cc0", checked: State.haslicense("cc0"), oninput: State.setlicense }),
-                    m("label[for=license-cc0]", State.licensefullname("cc0")),
-                    m("br"),
-                    m("input[type=checkbox]#license-by", { name: "licenses", value: "by", checked: State.haslicense("by"), oninput: State.setlicense }),
-                    m("label[for=license-by]", State.licensefullname("by")),
-                    m("br"),
-                    m("input[type=checkbox]#license-by-nc", { name: "licenses", value: "by-nc", checked: State.haslicense("by-nc"), oninput: State.setlicense }),
-                    m("label[for=license-by-nc]", State.licensefullname("by-nc")),
+                m("div#tabs", [
+                    m("span#packs_tab",
+                        {
+                            class: State.tab == "pack" ? "selected" : "",
+                            onclick: function () { State.tab = "pack" }
+                        },
+                        "Pack"
+                    ),
+                    m("span#speech_tab",
+                        {
+                            class: State.tab == "speech" ? "selected" : "",
+                            onclick: function () { State.tab = "speech" }
+                        },
+                        "Speech"
+                    ),
                 ]),
-                m("button", {
-                    onclick: State.retrieve
-                }, "Fetch pack"),
-
-                m("br"),
-                m("br"),
-
-                m("div.help", [
-                    m("img", { src: "assets/help.png", height: "32", title: "Help" }),
-
-                    m("p.explanation", [
-                        "Any word can be a pack definition.",
+                m("div.tabcontent", { style: State.tab == "pack" ? "display:block;" : "" }, [
+                    m("input[placeholder=Pack definition]#definition", {
+                        value: State.pack,
+                        oninput: function (e) { State.pack = e.target.value },
+                        onkeyup: function (e) {
+                            if (e.keyCode == 13) {
+                                State.retrieve()
+                            }
+                        }
+                    }),
+                    m("div#licenses", [
+                        m("input[type=checkbox]#license-cc0", { name: "licenses", value: "cc0", checked: State.haslicense("cc0"), oninput: State.setlicense }),
+                        m("label[for=license-cc0]", State.licensefullname("cc0")),
                         m("br"),
-                        "If you want more than one sample, separate words by a comma: blue,red",
+                        m("input[type=checkbox]#license-by", { name: "licenses", value: "by", checked: State.haslicense("by"), oninput: State.setlicense }),
+                        m("label[for=license-by]", State.licensefullname("by")),
                         m("br"),
-                        m("br"),
-                        "You can define how many variations of a sample to assemble by adding a colon and a number.",
-                        m("br"),
-                        "e.g. 'blue,red:3,yellow:2' will produce one 'blue' sample, three 'red' samples and two 'yellow' sample.",
-                        m("br"),
-                        m("br"),
-                        "In a hurry? You can directly include a pack in Estuary by executing in its terminal:",
-                        m("pre", '!reslist "' + location.href + 'blue,red.json"')
+                        m("input[type=checkbox]#license-by-nc", { name: "licenses", value: "by-nc", checked: State.haslicense("by-nc"), oninput: State.setlicense }),
+                        m("label[for=license-by-nc]", State.licensefullname("by-nc")),
                     ]),
+                    m("button", {
+                        onclick: State.retrieve
+                    }, "Fetch pack"),
+
+                    m("br"),
+                    m("br"),
+
+                    m("div.help", [
+                        m("img", { src: "assets/help.png", height: "32", title: "Help" }),
+
+                        m("p.explanation", [
+                            "Any word can be a pack definition.",
+                            m("br"),
+                            "If you want more than one sample, separate words by a comma: blue,red",
+                            m("br"),
+                            m("br"),
+                            "You can define how many variations of a sample to assemble by adding a colon and a number.",
+                            m("br"),
+                            "e.g. 'blue,red:3,yellow:2' will produce one 'blue' sample, three 'red' samples and two 'yellow' sample.",
+                            m("br"),
+                            m("br"),
+                            "In a hurry? You can directly include a pack in Estuary by executing in its terminal:",
+                            m("pre", '!reslist "' + location.href + 'blue,red.json"')
+                        ]),
+                    ])
+                ]),
+
+                m("div.clear"),
+
+                m("div.tabcontent", { style: State.tab == "speech" ? "display:block;" : "" }, [
+                    m("input[placeholder=Speech definition]#speechdefinition", {
+                        value: State.speech,
+                        oninput: function (e) { State.speech = e.target.value },
+                        onkeyup: function (e) {
+                            if (e.keyCode == 13) {
+                                State.retrievespeech()
+                            }
+                        }
+                    }),
+                    m("div#genders", [
+                        m("select#gender", { onchange: function (e) { State.gender = this.value } }, [
+                            m("option[value=f]", { selected: State.gender == "f" }, "Female"),
+                            m("option[value=m]", { selected: State.gender == "m" }, "Male")
+                        ]),
+                    ]),
+                    m("div#languages", [
+                        m('select#language', { onchange: function (e) { State.language = this.value }, value: State.language }, [
+                            m("option[value=af-ZA]", "Afrikaans"),
+                            m("option[value=ar-XA]", "Arabic"),
+                            m("option[value=bn-IN]", "Bengali"),
+                            m("option[value=bg-BG]", "Bulgarian"),
+                            m("option[value=ca-ES]", "Catalan"),
+                            m("option[value=yue-HK]", "Chinese (Hong Kong)"),
+                            m("option[value=cs-CZ]", "Czech"),
+                            m("option[value=da-DK]", "Danish"),
+                            m("option[value=nl-BE]", "Dutch (Belgium)"),
+                            m("option[value=nl-NL]", "Dutch (Netherlands)"),
+                            m("option[value=en-AU]", "English (Australia)"),
+                            m("option[value=en-IN]", "English (India)"),
+                            m("option[value=en-GB]", "English (UK)"),
+                            m("option[value=en-US]", "English (US)"),
+                            m("option[value=fil-PH]", "Filipino"),
+                            m("option[value=fi-FI]", "Finnish"),
+                            m("option[value=fr-CA]", "French (Canada)"),
+                            m("option[value=fr-FR]", "French (France)"),
+                            m("option[value=de-DE]", "German"),
+                            m("option[value=el-GR]", "Greek"),
+                            m("option[value=gu-IN]", "Gujarati"),
+                            m("option[value=hi-IN]", "Hindi"),
+                            m("option[value=hu-HU]", "Hungarian"),
+                            m("option[value=is-IS]", "Icelandic"),
+                            m("option[value=id-ID]", "Indonesian"),
+                            m("option[value=it-IT]", "Italian"),
+                            m("option[value=ja-JP]", "Japanese"),
+                            m("option[value=kn-IN]", "Kannada"),
+                            m("option[value=ko-KR]", "Korean"),
+                            m("option[value=lv-LV]", "Latvian"),
+                            m("option[value=ms-MY]", "Malay"),
+                            m("option[value=ml-IN]", "Malayalam"),
+                            m("option[value=cmn-CN]", "Mandarin Chinese"),
+                            m("option[value=mr-IN]", "Marathi"),
+                            m("option[value=nb-NO]", "Norwegian"),
+                            m("option[value=pl-PL]", "Polish"),
+                            m("option[value=pt-BR]", "Portuguese (Brazil)"),
+                            m("option[value=pt-PT]", "Portuguese (Portugal)"),
+                            m("option[value=pa-IN]", "Punjabi"),
+                            m("option[value=ro-RO]", "Romanian"),
+                            m("option[value=ru-RU]", "Russian"),
+                            m("option[value=sr-RS]", "Serbian"),
+                            m("option[value=sk-SK]", "Slovak"),
+                            m("option[value=es-ES]", "Spanish (Spain)"),
+                            m("option[value=es-US]", "Spanish (US)"),
+                            m("option[value=sv-SE]", "Swedish"),
+                            m("option[value=ta-IN]", "Tamil"),
+                            m("option[value=te-IN]", "Telugu"),
+                            m("option[value=th-TH]", "Thai"),
+                            m("option[value=tr-TR]", "Turkish"),
+                            m("option[value=uk-UA]", "Ukrainian"),
+                            m("option[value=vi-VN]", "Vietnamese")
+                        ])
+                    ]),
+                    m("button", {
+                        onclick: State.retrievespeech
+                    }, "Fetch speech"),
+
+                    m("br"),
+                    m("br"),
+
+                    m("div.help", [
+                        m("img", { src: "assets/help.png", height: "32", title: "Help" }),
+
+                        m("p.explanation", [
+                            "Any word can be a speech definition.",
+                            m("br"),
+                            "If you want more than one sample, separate words by a comma: blue,red",
+                            m("br"),
+                            m("br"),
+                            "You can use sentences by replacing spaces by underscores.",
+                            m("br"),
+                            "e.g. 'imagine_all,the_people' will produce a 'imagine all' sample and a 'the people' sample.",
+                            m("br"),
+                            m("br"),
+                            "In a hurry? You can directly include speech in Estuary by executing in its terminal:",
+                            m("pre", '!reslist "' + location.href + 'speech/blue,red.json?gender=f&language=en-GB"')
+                        ]),
+                    ])
                 ]),
 
                 m("div.pack-container", [
-                    m("h2", "Pack"),
+                    m("h2", "Samples"),
                     m("div.pack",
                         State.error ? m("span.error", State.error) :
                             State.progress ?
@@ -168,13 +337,22 @@ var Shabda = {
                                             soundlist = []
                                             for (index in State.lastreslistcontents) {
                                                 sound = State.lastreslistcontents[index]
-                                                soundlist.push(m('li', m('audio', { controls: true, src: sound.url }), " ", m('span.attribution', State.licensefullname(sound.licensename) + ' by ' + sound.author + ' ', m('a', { 'href': sound.original_url }, m('img', { src: 'assets/external-link.png' })))))
+                                                soundlist.push(
+                                                    m('li',
+                                                        m('audio', { controls: true, src: sound.url }),
+                                                        " ",
+                                                        m('span.attribution',
+                                                            sound.licensename ? State.licensefullname(sound.licensename) + ' by ' + sound.author + ' ' : '',
+                                                            sound.original_url ? m('a', { 'href': sound.original_url }, m('img', { src: 'assets/external-link.png' })) : ''
+                                                        )
+                                                    )
+                                                )
                                             }
                                             return soundlist
                                         }()),
                                         m("a.button", { href: State.lastretrievedzip() }, "Download all"),
                                     ]) :
-                                    m("i", "Fetch a pack to see its contents")
+                                    m("i", "Fetch a definition to hear its rendition")
                     )
                 ]),
             ),
